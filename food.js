ce10d7f083206796df46c133014fc1223c9c0c2b
Valentun2 <pocomaxawrkt@gmail.com>
2024-04-01 19:59:29 +0300

    Update food.js

--- food.js
+++ food.js
@@ -4,20 +4,21 @@ import { addSegment, snakeBody } from "./snake.js";
 const record = document.querySelector('.js-record')
 
  let scoresTotal = 0
-const maxScores = localStorage.getItem("maxScores") || 0
+let maxScores = localStorage.getItem("maxScores") ||0
 record.innerText = `Record: ${maxScores}`
 
 export let foodCoord = {x:7,y:15}
 
 export function updateFood() {
-
+   maxScores = localStorage.getItem("maxScores") ||0
    if(snakeBody[0].x === foodCoord.x && snakeBody[0].y === foodCoord.y){
       scoresTotal +=1
       scores.innerText = `Scores: ${scoresTotal}`
       if(maxScores <= scoresTotal){
+         console.log(maxScores,scoresTotal);
          record.innerText = `Record: ${scoresTotal}`
 
-         localStorage.setItem("maxScores", `${scoresTotal}`)
+         localStorage.setItem("maxScores", `${scoresTotal} `)
       }
       addSegment(foodCoord)
 foodCoord = {x:Math.floor(getRandomNumber()),y:Math.floor(getRandomNumber())}
